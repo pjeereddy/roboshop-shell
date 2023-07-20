6d6eafa2077837aa03616b69c2055b9144cdcde1
Pranayvreddy <jpranayviharreddy888@gmail.com>
2023-07-20 15:14:45 -0400

    I have ill created the file. Frontend server code

--- frontend.sh
+++ frontend.sh
@@ -2,3 +2,4 @@ yum install nginx -y
 systemctl enable nginx
 systemctl start nginx
 rm -rf /usr/share/nginx/html/*
+curl -o /tmp/frontend.zip https://roboshop-artifacts.s3.amazonaws.com/frontend.zip
