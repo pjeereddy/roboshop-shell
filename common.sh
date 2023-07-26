b2d2ab35c1707e9ac2b30c687ef9e331837a474a
Pranayvreddy <jpranayviharreddy888@gmail.com>
2023-07-26 12:33:17 -0400

    using functions and variables in user,shipping,cart

--- common.sh
+++ common.sh
@@ -1,3 +1,24 @@
+func_prerequisite(){
+ echo "\e[32m>>>>>>>>>>>>>>>> add user>>>>>>>>>>>>>>>> \e[0m"
+   useradd roboshop &>>${log}
+   echo "\e[32m>>>>>>>>>>>>>>>> cleanup old directory >>>>>>>>>>>>>>>> \e[0m"
+   rm -rf /app &>>${log}
+   echo "\e[32m>>>>>>>>>>>>>>>> create new directory >>>>>>>>>>>>>>>> \e[0m"
+   mkdir /app &>>${log}
+   echo "\e[32m>>>>>>>>>>>>>>>> download application content >>>>>>>>>>>>>>>> \e[0m"
+   curl -L -o /tmp/${component}.zip https://roboshop-artifacts.s3.amazonaws.com/${component}.zip &>>${log}
+
+   cd /app &>>${log}
+   echo "\e[32m>>>>>>>>>>>>>>>> unzip application content >>>>>>>>>>>>>>>> \e[0m"
+   unzip /tmp/${component}.zip &>>${log}
+
+   cd /app &>>${log}
+}
+func_systemd(){
+  systemctl daemon-reload &>>${log}
+  systemctl enable ${component} &>>${log}
+  systemctl restart ${component} &>>${log}
+}
 func_nodejs(){
 log=/tmp/roboshop.log
 echo -e "\e[32m<<<<<<<<<<<<<<<<< create ${component} service >>>>>>>>>>>>>>>>>>>>>\e[0m" | tee -a ${log}
@@ -8,17 +29,7 @@ echo -e '\e[32m<<<<<<<<<<<<<<<<< download nodejs setup >>>>>>>>>>>>>>>>>>\e[0m' 
 curl -sL https://rpm.nodesource.com/setup_lts.x | bash &>>${log}
 echo -e '\e[32m<<<<<<<<<<<<<<<<<< install nodejs repos >>>>>>>>>>>>>>>>>>>>>>>\e[0m' | tee -a ${log}
 yum install nodejs -y &>>${log}
-echo -e '\e[32m<<<<<<< add user >>>>>>>>>>>>\e[0m' | tee -a ${log}
-useradd roboshop &>>${log}
-echo -e '\e[32m<<<<<<<<< remove directory APP >>>>>>>>>>>>>>>>\e[0m' | tee -a ${log} | tee -a ${log}
-rm -rf /app &>>${log}
-echo -e '\e[32m<<<<<<<<<< create new directory APP >>>>>>>>>>>>>>\e[0m' | tee -a ${log}
-mkdir /app &>>${log}
-echo -e '\e[32m<<<<<<<<<<<<download application service >>>>>>>>>>>>>>>\e[0m' | tee -a ${log}
-curl -o /tmp/${component}.zip https://roboshop-artifacts.s3.amazonaws.com/${component}.zip &>>${log}
-cd /app &>>${log}
-unzip /tmp/${component}.zip &>>${log}
-cd /app &>>${log}
+func_prerequisite
 echo -e '\e[32m<<<<<<<<<<<< install dependencies >>>>>>>>>>>>>>>>\e[0m' | tee -a ${log}
 npm install &>>${log}
 echo -e '\e[32m<<<<<<<<<<< install mongodb shell repos >>>>>>>>>>>\e[0m' | tee -a ${log}
@@ -26,7 +37,25 @@ yum install mongodb-org-shell -y &>>${log}
 echo -e '\e[32m<<<<<<<<<<<< load schema >>>>>>>>>>>>>>>>>\e[0m' | tee -a ${log}
 mongo --host mongodb.jdevops74.online </app/schema/${component}.js &>>${log}
 echo -e '\e[32m<<<<<<<<<<<<<start service >>>>>>>>>>>>>>>\e[0m' | tee -a ${log}
-systemctl daemon-reload &>>${log}
-systemctl enable ${component} &>>${log}
-systemctl restart ${component} &>>${log}
+func_systemd
+}
+
+
+func_java(){
+  log=/tmp/roboshop.log
+  echo "\e[32m>>>>>>>>>>>>>>>> download ${component} application service >>>>>>>>>>>>>>>> \e[0m"
+  cp ${component}.service /etc/systemd/system/${component}.service &>>${log}
+  echo "\e[32m>>>>>>>>>>>>>>>> Install maven package >>>>>>>>>>>>>>>> \e[0m"
+  yum install maven -y &>>${log}
+  func_prerequisite
+  echo "\e[32m>>>>>>>>>>>>>>>> download dependencies >>>>>>>>>>>>>>>> \e[0m"
+  mvn clean package &>>${log}
+
+  mv target/${component}-1.0.jar ${component}.jar &>>${log}
+  echo "\e[32m>>>>>>>>>>>>>>>> install mysql>>>>>>>>>>>>>>>> \e[0m"
+  yum install mysql -y &>>${log}
+  echo "\e[32m>>>>>>>>>>>>>>>> Load schema >>>>>>>>>>>>>>>> \e[0m"
+  mysql -h mysql.jdevops74.online -uroot -pRoboShop@1 < /app/schema/${component}.sql &>>${log}
+  echo "\e[32m>>>>>>>>>>>>>>>> load service>>>>>>>>>>>>>>>> \e[0m"
+  func_systemd
 }
