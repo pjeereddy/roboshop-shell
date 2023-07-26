8546cabdea721912c351941a33c4818952275865
Pranayvreddy <jpranayviharreddy888@gmail.com>
2023-07-26 10:36:17 -0400

    appending the optputand error comments using "&>>/folder name"

--- catalogue.sh
+++ catalogue.sh
@@ -1,29 +1,29 @@
 echo -e '\e[31m<<<<<<<<<<<<<<<<< create catalogue service >>>>>>>>>>>>>>>>>>>>>\e[0m'
-cp catalogue.service /etc/systemd/system/catalogue.service
+cp catalogue.service /etc/systemd/system/catalogue.service &>>/temp/roboshop.log
 echo -e '\e[31m<<<<<<<<<<<<<<<<< create mongodb repo >>>>>>>>>>>>>>>>>>>\e[0m'
-cp mongo.repo /etc/yum.repos.d/mongo.repo
+cp mongo.repo /etc/yum.repos.d/mongo.repo &>>/temp/roboshop.log
 echo -e '\e[31m<<<<<<<<<<<<<<<<< download nodejs setup >>>>>>>>>>>>>>>>>>\e[0m'
-curl -sL https://rpm.nodesource.com/setup_lts.x | bash
+curl -sL https://rpm.nodesource.com/setup_lts.x | bash &>>/temp/roboshop.log
 echo -e '\e[31m<<<<<<<<<<<<<<<<<< install nodejs repos >>>>>>>>>>>>>>>>>>>>>>>\e[0m'
-yum install nodejs -y
+yum install nodejs -y &>>/temp/roboshop.log
 echo -e '\e[31m<<<<<<< add user >>>>>>>>>>>>\e[0m'
-useradd roboshop
+useradd roboshop &>>/temp/roboshop.log
 echo -e '\e[31m<<<<<<<<< remove directory APP >>>>>>>>>>>>>>>>\e[0m'
-rm -rf /app
+rm -rf /app &>>/temp/roboshop.log
 echo -e '\e[31m<<<<<<<<<< create new directory APP >>>>>>>>>>>>>>\e[0m'
-mkdir /app
+mkdir /app &>>/temp/roboshop.log
 echo -e '\e[31m<<<<<<<<<<<<download application service >>>>>>>>>>>>>>>\e[0m'
-curl -o /tmp/catalogue.zip https://roboshop-artifacts.s3.amazonaws.com/catalogue.zip
-cd /app
-unzip /tmp/catalogue.zip
-cd /app
+curl -o /tmp/catalogue.zip https://roboshop-artifacts.s3.amazonaws.com/catalogue.zip &>>/temp/roboshop.log
+cd /app &>>/temp/roboshop.log
+unzip /tmp/catalogue.zip &>>/temp/roboshop.log
+cd /app &>>/temp/roboshop.log
 echo -e '\e[31m<<<<<<<<<<<< install dependencies >>>>>>>>>>>>>>>>\e[0m'
-npm install
+npm install &>>/temp/roboshop.log
 echo -e '\e[31m<<<<<<<<<<< install mongodb shell repos >>>>>>>>>>>\e[0m'
-yum install mongodb-org-shell -y
+yum install mongodb-org-shell -y &>>/temp/roboshop.log
 echo -e '\e[31m<<<<<<<<<<<< load schema >>>>>>>>>>>>>>>>>\e[0m'
-mongo --host mongodb.jdevops74.online </app/schema/catalogue.js
+mongo --host mongodb.jdevops74.online </app/schema/catalogue.js &>>/temp/roboshop.log
 echo -e '\e[31m<<<<<<<<<<<<<start service >>>>>>>>>>>>>>>\e[0m'
-systemctl daemon-reload
-systemctl enable catalogue
-systemctl restart catalogue
+systemctl daemon-reload &>>/temp/roboshop.log
+systemctl enable catalogue &>>/temp/roboshop.log
+systemctl restart catalogue &>>/temp/roboshop.log
